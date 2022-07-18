ef780657f6afd785daa36687a36bd01fcc9d934d
mbpye <73852907+mbpye@users.noreply.github.com>
2022-07-18 03:06:42 +0300

    Add files via upload

---
+++ хуйя (удали)/хуйя (удали)/хуйя (удали).cpp
@@ -0,0 +1,29 @@
+﻿#include <cstdalign>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+string path = "logo.txt";
+
+ifstream fin;
+fin.open(path);
+
+if (!fin.is_open())
+{
+    cout << "error!!!!";
+
+}
+else
+{
+
+    char ch;
+    while (fin.get(ch))
+    {
+        cout << ch;
+    }
+}
+	system("pause");
+	return 0;
+}
